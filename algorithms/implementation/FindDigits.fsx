cff3b127656277980417857bbda9fee1825e6c41
yhylord <yhylord@gmail.com>
2016-06-09 00:17:17 +0800

    Solve Find Digits in Algorithms/Implementation

---
+++ algorithms/implementation/FindDigits.fsx
@@ -0,0 +1,12 @@
+﻿open System
+
+let solve () =
+    let s = Console.ReadLine()
+    let n = int s
+    s.ToCharArray()
+    |> Array.map (fun x -> (int x) - (int '0'))
+    |> Array.filter (fun x -> x <> 0 && n % x = 0)
+    |> Array.length
+
+let t = Console.ReadLine() |> int
+(printfn "%A", [ for i in 1 .. t do yield solve ()]) ||> List.map
