c409ada4ff84cd71a2f3c5b97f94d65b281e3322
yhylord <yhylord@gmail.com>
2016-06-08 21:37:25 +0800

    Solve Angry Professor in Algorithm/Implementation

---
+++ algorithms/implementation/AngryProfessor.fsx
@@ -0,0 +1,16 @@
+﻿open System
+
+let solve () =
+    let k = int (Console.ReadLine().Split()).[1]
+    let attendances =
+        Console.ReadLine().Split()
+        |> Array.map int
+        |> Array.filter (fun x -> x <= 0)
+        |> Array.length
+    match attendances with
+    | x when x < k -> printfn "YES"
+    | _ -> printfn "NO"
+
+let t = Console.ReadLine() |> int
+for i in 1..t do
+    solve ()
