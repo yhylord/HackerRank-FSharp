3d43b434f4bc01da2a448edc0b06bf0cfa30e976
yhylord <yhylord@gmail.com>
2016-06-08 23:46:21 +0800

    Solve Utopian Tree in Algorithms/Implementation

---
+++ algorithms/implementation/UtopianTreeBitwise.fsx
@@ -0,0 +1,15 @@
+﻿open System
+
+(*
+    Trivial to see that after n years (2n cycles),
+    the height = 2 ^ (n + 1) - 1,
+    which can be written as (1 <<< (x / 2 + 1)) - 1 (x in cycles).
+    The height after odd cycles doubles,
+    thus left shifted one bit ( <<< (x % 2) ).
+*)
+let t = Console.ReadLine() |> int
+for i in 1 .. t do
+    Console.ReadLine()
+    |> int
+    |> (fun x -> ((1 <<< (x / 2 + 1)) - 1) <<< (x % 2))
+    |> printfn "%A"
---
+++ algorithms/implementation/UtopianTreeRecursion.fsx
@@ -0,0 +1,14 @@
+﻿open System
+
+let solve () =
+    let n = Console.ReadLine() |> int64
+    let rec calc n =
+        match n with
+        | 0 -> 1
+        | x when x % 2 = 1 -> calc(n - 1) * 2
+        | _ -> calc(n - 1) + 1
+    calc n |> printfn "%d"
+
+let t = Console.ReadLine() |> int
+for i in 1 .. t do
+    solve ()
